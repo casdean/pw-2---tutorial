6cb0e623b66753d76d1ae539ad0bc199fe6383af
Maria Garceza <meduarda6660@gmail.com>
2025-03-13 10:58:25 -0300

    Add files via upload

---
+++ países/PW-06.03.25/include_require/china.php
@@ -0,0 +1,3 @@
+<?php include './header.php';?>
+<h1> China</h1>
+<?php include './footer.php';?>
---
+++ países/PW-06.03.25/include_require/footer.php
@@ -0,0 +1,22 @@
+<div class="row fixed-bottom">
+
+<div class="col-12">
+<p> Copyright @ 2025 -Todos os direitos reservados</p>
+
+
+</div>
+
+
+</div>
+
+
+
+
+
+
+<script src="./assets/js/bootstrap.min.css" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+
+
+</body>
+</html>
